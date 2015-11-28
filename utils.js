c9fb6795c444438a4bea7a2f5921a77b90e9fe32
rainyjune <rainyjune@live.cn>
2015-11-28 15:27:43 +0800

    Update utils.js

--- utils.js
+++ utils.js
@@ -42,13 +42,33 @@ exports.post = function(options, successCallback, errorCallback) {
   var mergedOptions = merge(defaultOption, options);
   var req = http.request(mergedOptions, function(res) {
     console.log('STATUS: ' + res.statusCode, 'HEADERS: ' + JSON.stringify(res.headers));
-    //res.setEncoding('utf8');
+
+    if (!mergedOptions.encoding || mergedOptions.encoding === "utf8") {
+      res.setEncoding('utf8');
+    }
+    
+    var resObj = res;
+    switch (res.headers['content-encoding']) {
+      case 'gzip':
+        var gunzip = zlib.createGunzip();    
+        res.pipe(gunzip);
+        resObj = gunzip;
+        break;
+      case 'deflate':
+        var inflat = zlib.createInflate();
+        res.pipe(inflat);
+        resObj = inflat;
+        break;
+      default:
+        break;
+    }
+  
     var chunks = [];
-    res.on('data', function (chunk) {
-      //console.log('BODY: ' + chunk);
+    
+    resObj.on('data', function (chunk) {
       chunks.push(chunk);
     });
-    res.on('end', function() {
+    resObj.on('end', function() {
       successCallback && successCallback(chunks.join(''));
     })
   });
@@ -59,77 +79,7 @@ exports.post = function(options, successCallback, errorCallback) {
   });
 
   // write data to request body
-  req.write(postData);
-  req.end();
-};
-
-exports.postData = function(opts, callback, errCallback) {
-  var postData = {
-    callCount: 1,
-    scriptSessionId: "${scriptSessionId}187",
-    "c0-scriptName": "BlogBeanNew",
-    "c0-methodName": "getBlogs",
-    "c0-id": 0,
-    "c0-param0": "number:171396050",
-    "c0-param1": "number:" + 10 * opts.page,
-    "c0-param2": "number:10",
-    "batchId": 307850
-  };
-  
-  var postDataArr = [];
-  for (var prop in postData) {
-    postDataArr.push(new String(prop) + '=' + postData[prop]);
-  }
-  
-  var postDataStr = postDataArr.join('\n');
-
-  var options = {
-    hostname: opts.hostname,
-    path: opts.path,
-    method: 'POST',
-    headers: {
-      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.86 Safari/537.36',
-      'Referer': 'http://api.blog.163.com/crossdomain.html?t=20100205',
-      'Accept': '*/*',
-      'Accept-Encoding': 'gzip, deflate',
-      'Accept-Language': 'en-US,en;q=0.8,zh-CN;q=0.6,zh;q=0.4,zh-TW;q=0.2,es;q=0.2',
-      'Cache-Control': 'no-cache',
-      'Connection': 'keep-alive',
-      'DNT': '1',
-      'Host': 'api.blog.163.com',
-      'Origin': 'http://api.blog.163.com',
-      'Pragma': 'no-cache',
-      'Content-Type': 'text/plain',
-      'Content-Length': postDataStr.length
-    }
-  };
-
-  var req = http.request(options, function(res) {
-    //console.log('STATUS: ' + res.statusCode);
-    //console.log('HEADERS: ' + JSON.stringify(res.headers));
-
-    var chunks = [];
-    
-    // pipe the response into the gunzip to decompress
-    var gunzip = zlib.createGunzip();            
-    res.pipe(gunzip);
-
-    gunzip.on('data', function (chunk) {
-      // decompression chunk ready, add it to the buffer
-      chunks.push(chunk.toString());
-    });
-    gunzip.on('end', function() {
-      callback && callback(chunks.join(''));
-    });
-  });
-
-  req.on('error', function(e) {
-    console.log('problem with request: ' + e.message);
-    errCallback && errCallback(e);
-  });
-  
-  // write data to request body
-  req.write(postDataStr);
+  req.write(options.dataString || postData);
   req.end();
 };
 
@@ -152,7 +102,7 @@ function merge() {
       for (prop in src) {
         if (src.hasOwnProperty(prop)) {
           if (toString.call(src[prop]) == '[object Object]') {
-            result[prop] = cp(result[prop] || {}, src[prop]);
+            result[prop] = merge(result[prop] || {}, src[prop]);
           } else {
             result[prop] = src[prop];
           }
