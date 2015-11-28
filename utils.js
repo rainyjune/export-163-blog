47a16d7431aa2474e2b7e67967b06aa75731501b
rainyjune <rainyjune@live.cn>
2015-11-28 14:17:34 +0800

    Add the post and get method

--- utils.js
+++ utils.js
@@ -1,51 +1,66 @@
-var http = require('http'), https = require('https'), url = require('url');
+var http = require('http'), url = require('url');
 var zlib = require("zlib");
 var iconv = require('iconv-lite'); // npm install iconv-lite
- 
-/**
- * Parse options 
- * @param  options
- * @return object
- */
-function parseOptions(options) {
-  var result = {protocol: 'http:', options: {}, encoding: 'utf8'};
-  if (typeof options == "string") {
-    result.options = options;
-    var urlComponent = url.parse(options);
-    result.protocol = urlComponent.protocol; 
-  } else if (typeof options == "object") {
-    result.options = options;
-    if (options.encoding && typeof options.encoding == "string") {
-      result.encoding = options.encoding;  
-    }
-    if (options.url && typeof options.url == "string") {
-      result.options = url.parse(options.url);
-    }
-    if (result.options.protocol) {
-      result.protocol = result.options.protocol;
-    }
-  }
-  return result;
-}
-exports.getHTML = function(options, callback, errCallback) {
-  var options = parseOptions(options);
-  var protocol = options.protocol == 'http:' ? http : https;
 
-  protocol.get(options.options, function(res) {
-    console.log("Got response: " + res.statusCode);
+/**
+ * Http Get Request.
+ *
+ *
+ */
+exports.get = function(options, successCallback, errorCallback) {
+  http.get(options, function(res){
     var chunks = [];
     res.on('data', function (chunk) {
       chunks.push(chunk);
     });
     res.on('end', function() {
-      var decodedBody = iconv.decode(Buffer.concat(chunks), options.encoding); // Convert from an encoded buffer to JavaScript string.
-      callback && callback(decodedBody);
+      // If the status code is not 200, call the errorCallback function.
+      if (res.statusCode !== 200)  {
+        errorCallback && errorCallback(new Error(res.statusMessage));
+        return ;
+      }
+      // Convert from an encoded buffer to JavaScript string if needed.
+      var decodedBody = options.encoding ? iconv.decode(Buffer.concat(chunks), options.encoding) : chunks.join(''); 
+      successCallback && successCallback(decodedBody);
     });
-    
-  }).on('error', function(e) {
-    console.log("Got error:" + e.message); 
-    errCallback && errCallback(e);
+  }).on('error', errorCallback);
+};
+
+/**
+ * @param {object} options: { hostname: '', path: '', data: {}, cookie: "", "encoding": "" }
+ *
+ * @TODO parameter validation.
+ */
+exports.post = function(options, successCallback, errorCallback) {
+  var postData = JSON.stringify(options.data);
+  var defaultOption = {
+    method: 'POST',
+    headers: {
+      'Content-Length': postData.length
+    }
+  };
+  var mergedOptions = merge(defaultOption, options);
+  var req = http.request(mergedOptions, function(res) {
+    console.log('STATUS: ' + res.statusCode, 'HEADERS: ' + JSON.stringify(res.headers));
+    //res.setEncoding('utf8');
+    var chunks = [];
+    res.on('data', function (chunk) {
+      //console.log('BODY: ' + chunk);
+      chunks.push(chunk);
+    });
+    res.on('end', function() {
+      successCallback && successCallback(chunks.join(''));
+    })
   });
+
+  req.on('error', function(e) {
+    console.log('problem with request: ' + e.message);
+    errorCallback && errorCallback(e.message);
+  });
+
+  // write data to request body
+  req.write(postData);
+  req.end();
 };
 
 exports.postData = function(opts, callback, errCallback) {
@@ -117,3 +132,53 @@ exports.postData = function(opts, callback, errCallback) {
   req.write(postDataStr);
   req.end();
 };
+
+/**
+ * Merge multiple objects dynamically with modifying either arguments.
+ * Inspired by http://stackoverflow.com/questions/171251#16178864
+ * @example merge(obj1, obj2, ....);
+ * @return {object} The merged object.
+ */
+function merge() {
+  var result = {}, 
+      src, 
+      prop, 
+      args = getArgumentsArray(arguments),
+      toString = Object.prototype.toString;
+      
+  while (args.length > 0) {
+    src = args.shift();
+    if (toString.call(src) === "[object Object]") {
+      for (prop in src) {
+        if (src.hasOwnProperty(prop)) {
+          if (toString.call(src[prop]) == '[object Object]') {
+            result[prop] = cp(result[prop] || {}, src[prop]);
+          } else {
+            result[prop] = src[prop];
+          }
+        }
+      }
+    }
+  }
+  return result;
+}
+
+/**
+ *
+ * Convert function arguments to an array.
+ * The arguments object can be converted to a real Array by using:
+ * var args = Array.prototype.slice.call(arguments);
+ * But it prevents optimizations in JavaScript engines(V8 for example).
+ * See: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/arguments
+ * @param {object} arguments The Array-like object.
+ * @return {array}
+ */
+function getArgumentsArray(arguments) {
+  var result = [],
+      len = arguments.length,
+      i;
+  for (i = 0; i < len; i++) {
+    result.push(arguments[i]);
+  }
+  return result;
+}
