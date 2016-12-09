83f0aa497a6f53ab3daef768ae1d4f7675f5e2f4
rainyjune <rainyjune@live.cn>
2016-12-09 20:01:34 +0800

    Use ES6 Promise

--- fire.js
+++ fire.js
@@ -2,7 +2,7 @@ var utils = require('./utils.js');
 var fs = require('fs');
 
 var count = 0; // How many articles in the blog?
-var pageSize = 100; // How many articles in each page? 
+var pageSize = 10; // How many articles in each page? 
 var pages = 0; // How many pages are there in the blog?
 
 if (!process.argv[2]) {
@@ -55,9 +55,27 @@ utils.get({ hostname: hostname, path: '/blog', encoding: 'GBK' }, function(res) 
   
   pages = Math.ceil(count / pageSize);
 
+  var listPagePromises = [];
   for ( var i = 0; i < pages; i++) {
-    getArticlesByPage(i);
+    listPagePromises.push(getListPagePromise(i));
   }
+  
+  Promise.all(listPagePromises).then(function(results) {
+    // All list pages loaded.
+    console.log('All list page promises done:', results);
+    var postLinks = [];
+    results.forEach(function(page) {
+      postLinks = postLinks.concat(page);
+    });
+    
+    console.log('All links:', postLinks.length);
+    
+    postLinks.forEach(function(postLink) {
+      getArticleByLink(postLink);
+    });
+  }).catch(function(error) {
+    console.error('list page error:', error);
+  });
   
   fs.exists(overviewFile, function (exists) {
     if(!exists) {
@@ -74,68 +92,59 @@ utils.get({ hostname: hostname, path: '/blog', encoding: 'GBK' }, function(res) 
   console.log('Get url ['+ hostname + '/blog] err:', e.message);
 });
 
-/**
- *
- * @param {number} page The page index, zero based. 
- */
-function getArticlesByPage(pageIndex) {
-  var postData = {
-    callCount: 1,
-    scriptSessionId: "${scriptSessionId}187",
-    "c0-scriptName": "BlogBeanNew",
-    "c0-methodName": "getBlogs",
-    "c0-id": 0,
-    "c0-param0": "number:" + userId,
-    "c0-param1": "number:" + (pageIndex * 10) ,
-    "c0-param2": "number:" + pageSize,
-    "batchId": 307850
-  };
-  var postDataArr = [];
-  for (var prop in postData) {
-    postDataArr.push(new String(prop) + '=' + postData[prop]);
-  }
-  var postDataStr = postDataArr.join('\n');
-  
-  utils.post({
-    hostname: hostname,
-    path: apiGetBlogPath,
-    encoding: 'GBK',
-    data: postData,
-    dataString: postDataStr,
-    headers: {
-      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.86 Safari/537.36',
-      'Referer': 'http://api.blog.163.com/crossdomain.html?t=20100205',
-      'Accept': '*/*',
-      'Accept-Encoding': 'gzip, deflate',
-      'Accept-Language': 'en-US,en;q=0.8,zh-CN;q=0.6,zh;q=0.4,zh-TW;q=0.2,es;q=0.2',
-      'Cache-Control': 'no-cache',
-      'Connection': 'keep-alive',
-      'DNT': '1',
-      'Host': 'api.blog.163.com',
-      'Origin': 'http://api.blog.163.com',
-      'Pragma': 'no-cache',
-      'Content-Type': 'text/plain',
-      'Content-Length': postDataStr.length
+function getListPagePromise(pageIndex) {
+  return new Promise(function(resolve, reject) {
+    var postData = {
+      callCount: 1,
+      scriptSessionId: "${scriptSessionId}187",
+      "c0-scriptName": "BlogBeanNew",
+      "c0-methodName": "getBlogs",
+      "c0-id": 0,
+      "c0-param0": "number:" + userId,
+      "c0-param1": "number:" + ((pageIndex > 0) ? (pageIndex + 1) * 10 : 0) ,
+      "c0-param2": "number:" + (pageIndex > 0 ? 10 : 20),
+      "batchId": 307850
+    };
+    var postDataArr = [];
+    for (var prop in postData) {
+      postDataArr.push(new String(prop) + '=' + postData[prop]);
     }
-  }, function(res){
-    //console.log("post ok:", res);
-    getBlogLinks(res, postData);
-  }, function(e) {
-    console.log('post err:', e.message);
-  });
-}
-
-function getBlogLinks(htmlString, postData) {
-  var pattern = /permalink\s*=\s*"blog\/static\/\d+";/g;
-  var links =  htmlString.match(pattern);
-  //console.log('find match links:', links);
-  //return;
-  if (!links) {
-    console.warn("Links not found.", postData);
-    return ;
-  }
-  links.forEach(function(item, index){
-    getArticleByLink(item);
+    var postDataStr = postDataArr.join('\n');
+    
+    utils.post({
+      hostname: hostname,
+      path: apiGetBlogPath,
+      encoding: 'GBK',
+      data: postData,
+      dataString: postDataStr,
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.86 Safari/537.36',
+        'Referer': 'http://api.blog.163.com/crossdomain.html?t=20100205',
+        'Accept': '*/*',
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'en-US,en;q=0.8,zh-CN;q=0.6,zh;q=0.4,zh-TW;q=0.2,es;q=0.2',
+        'Cache-Control': 'no-cache',
+        'Connection': 'keep-alive',
+        'DNT': '1',
+        'Host': 'api.blog.163.com',
+        'Origin': 'http://api.blog.163.com',
+        'Pragma': 'no-cache',
+        'Content-Type': 'text/plain',
+        'Content-Length': postDataStr.length
+      }
+    }, function(res){
+      
+      var pattern = /permalink\s*=\s*"blog\/static\/\d+";/g;
+      var links =  res.match(pattern);
+      if (!links) {
+        console.warn("Links not found.", postData);
+        resolve([]);
+      } else {
+        resolve(links);
+      }
+    }, function(e) {
+      reject(e);
+    });
   });
 }
 
